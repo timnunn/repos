ab96aee29dbcd27dfc344631c7d0b19c0717621a
Timothy Nunn <54078499+timnunn@users.noreply.github.com>
2020-04-30 22:06:07 -0400

    Add files via upload

---
+++ nunn-assignment-4-generics/nunn-assignment-4-generics/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using static System.Console;
+using System.Collections.Generic;
+
+namespace nunn_assignment_4_generics
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+
+            ReadKey();
+        }
+    }
+}
