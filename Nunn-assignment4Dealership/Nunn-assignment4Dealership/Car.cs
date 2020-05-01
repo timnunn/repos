16e3a4e177db8db850b694d9a2e67d2e7be16680
Timothy Nunn <54078499+timnunn@users.noreply.github.com>
2020-04-30 22:07:28 -0400

    Add files via upload

---
+++ Nunn-assignment4Dealership/Nunn-assignment4Dealership/Car.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Nunn_assignment4Dealership
+{
+    public class Car : Vehicle
+
+    {
+        public new void Drive() => WriteLine($"{name} has just been waxed.");
+        public Car(string newName) : base(newName) { }
+
+        public override void MakeNoise()
+        {
+            WriteLine($"{name} says Rumble!");
+        }
+    }
+}
---
+++ Nunn-assignment4Dealership/Nunn-assignment4Dealership/Dealership.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nunn_assignment4Dealership
+{
+    public class Dealership<T> : IEnumerable<T>
+        where T : Vehicle
+    {
+        private List<T> vehicle { get; } = new List<T>();
+        public List<T> Vehicles
+        {
+            get { return vehicle; }
+        }
+        public IEnumerator<T> GetEnumerator() => vehicle.GetEnumerator();
+        IEnumerator IEnumerable.GetEnumerator() => vehicle.GetEnumerator();
+        public void MakeNoises()
+        {
+            foreach (T vehicle in Vehicles)
+            {
+                vehicle.MakeNoise();
+            }
+        }
+        public void DriveTheVehicles()
+        {
+            foreach (T vehicle in Vehicles)
+            {
+                vehicle.Drive();
+            }
+        }
+        public Dealership<Truck> GetTrucks()
+        {
+            Dealership<Truck> truckDealership = new Dealership<Truck>();
+            foreach (T vehicle in Vehicles)
+            {
+                if (vehicle is Truck)
+                {
+                    truckDealership.Vehicles.Add(vehicle as Truck);
+                }
+            }
+            return truckDealership;
+        }
+    }
+}
