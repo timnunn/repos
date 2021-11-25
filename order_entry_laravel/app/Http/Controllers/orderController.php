d4262a811872a4ea72de84065d40a73147a95ec9
Timothy Nunn <54078499+timnunn@users.noreply.github.com>
2021-11-25 17:36:11 -0500

    Add files via upload

---
+++ order_entry_laravel/app/Http/Controllers/orderController.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Order;
+use Illuminate\Http\Request;
+
+class orderController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        $order = order::latest()->paginate(5);
+
+        return view('order.index', compact('order'))
+        ->with('i', (request()->input('page', 1) - 1) * 5);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('order.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'Lname' => 'required',
+            'Item' => 'required',
+            'Qty' => 'required'
+        ]);
+
+        Product::create($request->all());
+
+        return redirect()->route('order.index')
+            ->with('success', 'Order created successfully.');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Order  $order
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Order $order)
+    {
+        return view('order.show', compact('order'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Order  $order
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Order $order)
+    {
+        return view('order.edit', compact('order'));
+    }
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\order  $order
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Order $order)
+    {
+        $request->validate([
+            'Lname' => 'required',
+            'Item' => 'required',
+            'Qty' => 'required'
+        ]);
+        $order->update($request->all());
+
+        return redirect()->route('order.index')
+            ->with('success', 'Order updated successfully');
+    }
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Order  $order
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Order $order)
+    {
+        $order->delete();
+
+        return redirect()->route('order.index')
+            ->with('success', 'Order deleted successfully');
+    }
+}
+
+
